4b1afb129a1d35d0d02c854d27bc2a1d632e8dbe
yoon00 <y00ndlek@gmail.com>
2025-05-28 15:20:32 +0900

    first commit

---
+++ client/main.js
@@ -0,0 +1,4 @@
+console.log(navigator.language);
+// console.log(process.env)
+
+const a = 10;
---
+++ server/index.js
@@ -0,0 +1,11 @@
+// console.log(process.env.lang)
+
+const server = require("live-server")
+const params = {
+  host: "localhost",
+  port: 3000,
+  open: false,
+  root: "./client",
+}
+
+server.start(params)
