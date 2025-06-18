269b06a73597ebb3b85b9e32d1fe9f9d4ad6d828
yoon00 <y00ndlek@gmail.com>
2025-06-18 10:00:10 +0900

    0617_randomDice

--- client/lib/utils/copy.js
+++ client/lib/utils/copy.js
@@ -1,3 +1,10 @@
+
+
 export function copy(text){
-    return navigator.clipboard.writeText(text)
+   return navigator.clipboard.writeText(text)
 }
+
+
+
+
+
--- client/lib/utils/index.js
+++ client/lib/utils/index.js
@@ -4,3 +4,4 @@
 
 export * from './type.js'
 export * from './copy.js'
+export * from './memo.js'
